b6bf479f36cdf3f9a2a1cdf4dce79273ce50f0e3
danielguerra69 <daniel.guerra69@gmail.com>
2016-11-03 01:46:56 +0100

    change to eth1

--- bin/dumpmitmproxync.sh
+++ bin/dumpmitmproxync.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mitmdump &
-tshark -o 'ssl.desegment_ssl_records: TRUE' -o 'ssl.desegment_ssl_application_data: TRUE' -o 'ssl.keys_list: $HOSTNAME,8080,http,/root/.mitmproxy/mitmproxy-ca.pem' -o 'ssl.debug_file: /root/wireshark-log' -i eth0   -w - 'tcp and port 8080' | nc $1 $2
+tshark -o 'ssl.desegment_ssl_records: TRUE' -o 'ssl.desegment_ssl_application_data: TRUE' -o 'ssl.keys_list: $HOSTNAME,8080,http,/root/.mitmproxy/mitmproxy-ca.pem' -o 'ssl.debug_file: /root/wireshark-log' -i eth1   -w - 'tcp and port 8080' | nc $1 $2
