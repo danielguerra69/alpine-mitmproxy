1ddff259e1726ecbab51da8a2901d01ae23cccb6
danielguerra69 <daniel.guerra69@gmail.com>
2016-11-03 09:52:21 +0100

    Revert "add ssl to decrypt"
    This reverts commit 20ac83f7a74dd5d26d237cbb68b94b56e2b022ed.

--- bin/dumpmitmproxync.sh
+++ bin/dumpmitmproxync.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mitmdump &
-tshark -o 'ssl.desegment_ssl_records: TRUE' -o 'ssl.desegment_ssl_application_data: TRUE' -o 'ssl.keys_list: $HOSTNAME,8080,ssl && http,/root/.mitmproxy/sslkeylog' -o 'ssl.debug_file: /root/tshark.log' -i eth0   -w - 'tcp and port 8080' | nc $1 $2
+tshark -o 'ssl.desegment_ssl_records: TRUE' -o 'ssl.desegment_ssl_application_data: TRUE' -o 'ssl.keys_list: $HOSTNAME,8080,http,/root/.mitmproxy/sslkeylog' -o 'ssl.debug_file: /root/tshark.log' -i eth0   -w - 'tcp and port 8080' | nc $1 $2
